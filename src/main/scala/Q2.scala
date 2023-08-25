bfdb6088868bd50aca5ab1fcf2075711db74a900
Dewranga Bandara <dewrangabandara071@gmail.com>
2023-08-25 09:59:02 +0530

    Initial commit 1

--- src/main/scala/Q2.scala
+++ src/main/scala/Q2.scala
@@ -14,11 +14,11 @@ object Q2 {
     val n3 = readInt()
     print("Enter the third denominator: ")
     val d3 = readInt()
-    val rational = new Rational(n1, d1, n2, d2)
-    rational.sub()
-    val rational1 = new Rational(rational.sub()._1, rational.sub()._2, n3, d3)
-    rational1.sub()
-    println(s"x = $n1/$d1 , y = $n2/$d2 , z = $n3/$d3 \nx-y-z = ${rational1.sub()._1}/${rational1.sub()._2}")
+    val rational1 = new Rational(n1, d1, n2, d2)
+    val result = rational1.sub()
+    val rational2 = new Rational(result._1, result._2, n3, d3)
+    val finalResult = rational2.sub()
+    println(s"x = $n1/$d1 , y = $n2/$d2 , z = $n3/$d3 \nx-y-z = ${finalResult._1}/${finalResult._2}")
 
   }
 }
--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -17,14 +17,10 @@ class Rational(n1: Int, d1: Int, n2: Int, d2: Int) {
   def sub() : (Int,Int) = {
     val n3 = (n1 * d2) - (n2 * d1)
     val d3 = d1 * d2
-    var i = 1
-    if(n3 < 0 || d3 < 0){
-      i = -1
-    }
     val gcdVal: Int = gcd(n3.abs, d3.abs)
     val numerator: Int = n3 / gcdVal
     val denominator: Int = d3 / gcdVal
-    (numerator*i, denominator)
+    (numerator, denominator)
   }
 
 }
